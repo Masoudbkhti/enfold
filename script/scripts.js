77af436ed2eae6a0a5b6954dabf92487498eaaad
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-05-08 16:41:43 +0430

    fixed bug - accordion without transition

--- script/scripts.js
+++ script/scripts.js
@@ -1,42 +1,6 @@
 //TOURDATES ACOORDION
-// const acoordionTitle = document.querySelector(".tour-dates__acoordion");
-// function openAcoordion() {
-//   acoordionTitle.style.height = "200px";
-// }
-// function closeAcoordion() {
-//   acoordionTitle.style.height = "auto";
-// }
 
-// پیدا کردن تمام آکاردئون ها با کلاس "list-header"
-// var accordions = document.querySelectorAll(".list-header");
-
-// // پیمایش تمام المان ها و اضافه کردن رویداد کلیک به هر کدام
-// for (var i = 0; i < accordions.length; i++) {
-//   accordions[i].addEventListener("click", function () {
-//     // پیدا کردن المان های آکاردئون داخل المان کلیک شده
-//     var content = this.nextElementSibling;
-
-//     // بررسی وضعیت آکاردئون بسته شده است یا باز؟
-//     if (content.style.maxHeight) {
-//       // اگر باز بود ، بسته شود
-//       content.style.maxHeight = null;
-//       this.innerHTML = "+ " + this.innerHTML.slice(2);
-//     } else {
-//       // اگر بسته بود ، باز شود
-//       // بستن تمام آکاردئون های دیگر باز
-//       for (var j = 0; j < accordions.length; j++) {
-//         var otherContent = accordions[j].nextElementSibling;
-//         if (otherContent.style.maxHeight) {
-//           otherContent.style.maxHeight = null;
-//           accordions[j].innerHTML = "+ " + accordions[j].innerHTML.slice(2);
-//         }
-//       }
-//       // باز کردن آکاردئون کلیک شده
-//       content.style.maxHeight = content.scrollHeight + "px";
-//       this.innerHTML = "- " + this.innerHTML.slice(2);
-//     }
-//   });
-// }
+/*
 
 const accordionHeaders = document.querySelectorAll(".list-header");
 const accordionContents = document.querySelectorAll(".list-content");
@@ -50,8 +14,56 @@ function toggleAccordion() {
     accordionContent.style.display = "none";
     this.textContent = this.textContent.replace("-", "+");
   } else {
+    accordionContents.forEach((content) => {
+      if (content.classList.contains("active")) {
+        content.classList.remove("active");
+        content.style.display = "none";
+        content.previousElementSibling.classList.remove("active");
+        content.previousElementSibling.textContent =
+          content.previousElementSibling.textContent.replace("-", "+");
+      }
+    });
+
     accordionContent.style.display = "block";
     this.textContent = this.textContent.replace("+", "-");
+  }
+}
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", toggleAccordion);
+});
+
+*/
+
+const accordionHeaders = document.querySelectorAll(".list-header");
+const accordionContents = document.querySelectorAll(".list-content");
+
+function toggleAccordion() {
+  const accordion = this.parentElement;
+  const accordionContent = accordion.querySelector(".list-content");
+  const isActive = accordionContent.classList.contains("active");
+
+  // Close all active accordion contents
+  accordionContents.forEach((content) => {
+    if (content.classList.contains("active") && content !== accordionContent) {
+      content.style.display = "none";
+      content.classList.remove("active");
+      const header = content.previousElementSibling;
+      header.classList.remove("active");
+      header.querySelector(".toggle-icon").textContent = "+";
+    }
+  });
+
+  if (isActive) {
+    accordionContent.style.display = "none";
+    accordionContent.classList.remove("active");
+    this.classList.remove("active");
+    this.querySelector(".toggle-icon").textContent = "+";
+  } else {
+    accordionContent.style.display = "block";
+    accordionContent.classList.add("active");
+    this.classList.add("active");
+    this.querySelector(".toggle-icon").textContent = "-";
   }
 }
 
